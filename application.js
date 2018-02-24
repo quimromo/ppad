2ad12ef51baa419904d2f1c086f31a85a5f32459
Quim Romo <quimromo@gmail.com>
2018-02-25 00:55:58 +0100

    wip practica ppad

--- application.js
+++ application.js
@@ -41,9 +41,9 @@ function loadFromArray(obj, firstIdx, dataArray){
     return idx;
 }
 
-var countries = {};
-function onDataLoaded(data){
-
+function parseDatabaseData(data){
+    
+    var countries = {};
     var parsedData = $.csv.toArrays(data, {separator : ";"});
     console.log(parsedData);
     var beginData = 4;
@@ -53,61 +53,131 @@ function onDataLoaded(data){
         countries[countryData[0]] = CountryData.fromDataArray(countryData);
     }
 
-    console.log(countries);
+    return countries;
 
 
 
-    google.charts.load('current', {
-        'packages':['geochart'],
-        // Note: you will need to get a mapsApiKey for your project.
-        // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-      });
-      google.charts.setOnLoadCallback(drawRegionsMap);
+    // google.charts.load('current', {
+    //     'packages':['geochart'],
+    //     // Note: you will need to get a mapsApiKey for your project.
+    //     // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    //     'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+    //   });
+    //   google.charts.setOnLoadCallback(drawRegionsMap);
     
-        var displayedData = [['Country', 'Agriculture Male 2016', 'Agriculture Female 2016' ]];
-        for( var country in countries ){
-            displayedData.push( [country, countries[country].agriculture.male["2016"],
-            countries[country].agriculture.female["2016"]
-        ] );
-        }
+    //     var displayedData = [['Country', 'Agriculture Male 2016', 'Agriculture Female 2016' ]];
+    //     for( var country in countries ){
+    //         displayedData.push( [country, countries[country].agriculture.male["2016"],
+    //         countries[country].agriculture.female["2016"]
+    //     ] );
+    //     }
 
 
-      function drawRegionsMap() {
-        var data = google.visualization.arrayToDataTable(displayedData);
+    //   function drawRegionsMap() {
+    //     var data = google.visualization.arrayToDataTable(displayedData);
       
-        var options = {};
+    //     var options = {};
     
-        var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+    //     var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
     
-        chart.draw(data, options);
-      }
+    //     chart.draw(data, options);
+    //   }
 }
 //https://experiments.withgoogle.com/chrome/globe
-var geojson = {};
-var countryGeometry = {};
-function onGeoJsonLoaded(data){
-    geojson = JSON.parse(data);
+
+function parseGeoJsonData(data){
+    var countryGeometry = {};
+    var geojson = JSON.parse(data);
     for(var feature of geojson.features){
         countryGeometry[feature.properties.sovereignt] = feature.geometry;
     }
-    console.log(countryGeometry);
+    return countryGeometry;
 }
 
 var promiseDatabase = new Promise(function(resolve){
     getFile("2.3_Employment_by_sector.txt", function(data){
-        onDataLoaded(data);
-        resolve(countries);
+        resolve(parseDatabaseData(data));
     }.bind(this) );
 }.bind(this));
 
 var promiseGeometry = new Promise(function(resolve){
     getFile("geojson_data.json", function(data){
-        onGeoJsonLoaded(data);
-        resolve(countryGeometry);
+        resolve(parseGeoJsonData(data));
     }.bind(this));
 }.bind(this));
 
+var countryData = {};
 var all = Promise.all([promiseDatabase, promiseGeometry]).then(function([database, geometry]){
-    
+    var numEntriesInDB = Object.keys(database).length;
+    var numEntriesInGeometry = Object.keys(geometry).length;
+    console.log( "Num entries DB: " + numEntriesInDB);
+    console.log( "Num entries Geom: " + numEntriesInGeometry);
+    for( var geo in geometry){
+        if( database[geo]){
+            countryData[geo] = { "data" : database[geo], "geometry" : geometry[geo], "points" : [] };
+        }
+        else{
+            var geoLC = geo.toLowerCase();
+            var geoLCParts = geoLC.split(" ");
+            var found = false;
+            for(var name in database){
+                var nameLC = name.toLowerCase();
+                if(nameLC.includes(geoLC) || geoLC.includes(nameLC) ){
+                    found = true;
+                    countryData[name] = {"data" : database[name], "geometry" : geometry[geo], "points" : [] };
+                    break;
+                }
+                // var allParts = true;
+                // for( var geoPart of geoLCParts){
+                //     if(!name.includes(geoPart)){
+                //         allParts = false;
+                //         break;
+                //     }
+                // }
+                // if(!allParts){
+                //     allParts = true;
+                //     var nameParts = name.split(" ");
+                //     for( var part of nameParts){
+                //         if(!geoLC.includes(part)){
+                //             allParts = false;
+                //             break;
+                //         }
+                //     }
+                // }
+                // found = allParts;
+
+            }
+            if( !found ){
+                console.log( "Not found country: " + geo + " in DB");
+            }
+        }
+    }
+
+    var latitudeSamples = 180;
+    var longitudeSamples = 360;
+    var minLat = -90.0;
+    var maxLat = 90.0;
+    var minLong = 0.0;
+    var maxLong = 360.0;
+
+    var points = [];
+    for( var i = 0; i <= latitudeSamples; ++i){
+        var lat = minLat + (maxLat - minLat) * (i/latitudeSamples);
+        for( var j = 0; j < longitudeSamples; ++j){
+            var long = minLong + (maxLong - minLong) * (j/longitudeSamples);
+            points.push( [long, lat] );
+        }
+    }
+
+    for(var p of points){
+        for( var country in countryData){
+            if( gju.pointInPolygon({"type":"Point","coordinates":p}, countryData[country].geometry ) ){
+                countryData[country].points.push(p);
+                break;
+            }
+        }
+    }
+    console.log(countryData);
+    //console.log(points);
+
 });
